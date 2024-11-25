32624efd28ce331273d96e5448468c37d6d59c9b
NIVAS-kr <20bcs1180@cuchd.in>
2024-11-25 16:38:40 +0530

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -27,6 +27,7 @@ exports.login = async (req, res) => {
      const options = {
        expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
        httpOnly: true,
+       sameSite: None
      };
      return res.status(200).cookie("token", token, options).json({
        success: true,
