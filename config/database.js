86282ae20709caf9408ca6741e433a01ed8c3bbd
NIVAS-kr <20bcs1180@cuchd.in>
2024-11-25 15:56:17 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,10 +4,7 @@ const mongoose = require('mongoose');
 exports.connectDatabase = () => {
     const uri = process.env.MONGO_URI; // Ensure this matches your environment variable name
     mongoose
-        .connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+        .connect(uri)
         .then(() => console.log('Connected to MongoDB'))
         .catch((err) => console.error('MongoDB connection error:', err));
 };
